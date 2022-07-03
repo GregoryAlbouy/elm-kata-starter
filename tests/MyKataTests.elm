6dcc3026135350779c34877c961c97ed1f66c945
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-03 18:19:06 +0200

    Set up test environment

---
+++ tests/MyKataTests.elm
@@ -0,0 +1,16 @@
+module MyKataTests exposing (..)
+
+import Expect
+import Fuzz exposing (string)
+import MyKata
+import Test exposing (Test, describe, fuzz, test)
+
+
+testIdentity : Test
+testIdentity =
+    describe "MyKata.identity"
+        [ test "returns input Bool parameter" <|
+            \_ -> Expect.equal True (MyKata.identity True)
+        , fuzz string "returns input String parameter" <|
+            \str -> Expect.equal str (MyKata.identity str)
+        ]
